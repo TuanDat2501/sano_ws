8a9863ee9133d5e8c6c1b6f032f5d6514d868c99
TuanDat2501 <169238329+TuanDat2501@users.noreply.github.com>
2026-05-05 14:40:13 +0700

    0505 dashboard

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -20,7 +20,7 @@ export default function DashboardPage() {
 
     const currentUser = session?.user as any;
     const userRole = currentUser?.role || "CONTENT";
-    const isManager = ["LEADER", "BAN_GIAM_DOC", "ADMIN"].includes(userRole);
+    const isManager = ["LEADER", "BAN_GIAM_DOC", "ADMIN","HR"].includes(userRole);
 
     return (
         // Tối ưu Padding: Mobile p-4, Tablet/PC p-8
